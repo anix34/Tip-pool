809223bd593ed6a6221b36c4f70bdb8d87301131
anix34 <139282344+anix34@users.noreply.github.com>
2023-07-22 19:44:21 -0400

    Add files via upload

---
+++ helpers.test.js
@@ -0,0 +1,65 @@
+describe('Helpers.js - Calculation Tests and DOM & Global Var Manipulation', () => {
+    beforeAll(() => {
+      billAmtInput.value = 249.99;
+      tipAmtInput.value = 11;
+      serverNameInput.value = 'Alice';
+      submitPaymentInfo();
+      submitServerInfo();
+    })
+    // it('should calculate the aggregate tip amount on all payments through sumPaymentTotal())', () => {
+    //   expect(sumPaymentTotal('tipAmt', 11.00)).toEqual(11.00);
+    //   expect(sumPaymentTotal('billAmt', 249.99)).toEqual(249.99);
+    
+    // })
+  
+    it('should return the tip percentage based on bill and tip amount through calculateTipPercent()', () => {
+      expect(calculateTipPercent(100, 10)).toEqual(10);
+      expect(calculateTipPercent(1000, 123.99)).toEqual(12);
+    })
+  
+    it('should remove a payment from the allPayments object on deleteFromGlobals()', () => {
+      deleteFromGlobals(Object.keys(allPayments)[0]);
+      expect(Object.keys(allPayments).length).toEqual(0);
+    })
+    it('should remove a server from the allServers object on deleteFromGlobals()', () => {
+      deleteFromGlobals(Object.keys(allServers)[0]);
+      expect(Object.keys(allServers).length).toEqual(0);
+    })
+  
+    afterAll(() => {
+      billAmtInput.value = '';
+      tipAmtInput.value = '';
+      allPayments = {};
+      paymentId = 0;
+      serverNameInput.value = '';
+      allServers = {};
+      serverTbody.innerHTML = '';
+      serverId = 0;
+      paymentTbody.innerHTML = '';
+      for (td of summaryTds) {
+        td.innerText = '';
+      }
+    })
+  })
+  
+  describe('Helpers.js - HTML Element Creation Tests', () => {
+    it('should append tds to a provided TR through appendTd()', () => {
+      let testTR = document.createElement('tr');
+      let testValues = ['', 'batman', '123', 12, true];
+  
+      for (value of testValues) {
+        appendTd(testTR, value)
+      }
+  
+      let testTDs = testTR.querySelectorAll('td');
+      expect(testTDs.length).toEqual(testValues.length);
+    })
+  
+    it('should append a tds w/ nested delete button to a provided TR through appendDeleteBtn()', () => {
+      let testTR = document.createElement('tr');
+      appendDeleteBtn(testTR);
+      let testTrTd = testTR.querySelector('td');
+      expect(testTrTd.innerText).toEqual('x');
+      expect(testTrTd.childNodes[0].tagName).toEqual('BUTTON');
+    })
+  })
---
+++ server.test.js
@@ -0,0 +1,38 @@
+describe("Servers test (with setup and tear-down)", () => {
+    beforeEach(() => {
+      serverNameInput.value = 'Alice';
+    });
+  
+    it('should add a new server to allServers on submitServerInfo()', () => {
+      submitServerInfo();
+  
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
+  
+    it('should not work if server name is empty "", when submitServerInfo() is called', () => {
+      serverNameInput.value = "";
+      submitServerInfo();
+      expect(Object.keys(allServers).length).toEqual(0);
+    })
+  
+    it('should add a new TR to the serverTable on UpdateServerTable()', () => {
+      submitServerInfo(); // updateServerTable() is called within submitServerInfo
+  
+      const serverRowNodes = serverTbody.querySelectorAll('tr');
+      expect(serverRowNodes.length).toEqual(1);
+  
+      const serverRowCols = serverRowNodes[0].querySelectorAll('td');
+      expect(serverRowCols.length).toEqual(3);
+      expect(serverRowCols[0].innerText).toEqual('Alice');
+      expect(serverRowCols[1].innerText).toEqual('$0.00');
+      expect(serverRowCols[2].innerText).toEqual('x');
+    })
+  
+    afterEach(() => {
+      serverNameInput.value = '';
+      allServers = {};
+      serverTbody.innerHTML = '';
+      serverId = 0;
+    });
+  });
